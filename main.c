89c6422b2a473886ad939c1dddb6d3dd80fee386
0199kye <70757090+0199kye@users.noreply.github.com>
2020-11-19 15:49:27 +0900

    submission 4

--- main.c
+++ main.c
@@ -6,13 +6,11 @@
 
 int main(int argc, char *argv[]) {
 	
-	char src[] = "The worst things to to eat before you sleep";
-	char dst[100];
 	
-	strcpy(dst, src);
+	char str[30] = "happy c programming";
 	
-	printf ("copied string: %s", dst);
-	
+	printf("문자열\"%s\"의 길이: %i\n", str, strlen(str) );
+
 	return 0; 
 }
 
